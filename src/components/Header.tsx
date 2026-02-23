b7f352c3a072ef8246cb04b3c6a18a116ad48912
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2026-02-23 09:21:15 +0100

    style(header): refine logo link layout and profile divider

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,14 +10,14 @@ function Header() {
     <header className="bg-primary/65 border-subtle/20 shadow-elevation-low fixed z-10 w-full border-b border-solid px-8 py-[1.2188rem] backdrop-blur-lg">
       <nav className="flex min-h-10 flex-col items-center justify-between gap-4 text-base font-medium md:flex-row">
         <ul className="flex flex-wrap items-center justify-center gap-x-7 gap-y-2 md:gap-x-8">
-          <li className="basis-full md:basis-auto">
+          <li className="flex-center basis-full md:basis-auto">
             <NavLink
-              className="focus-visible:ring-accent bg-primary focus-visible:ring-offset-primary block rounded-2xl p-2 transition-all duration-200 ease-in-out focus-visible:leading-none focus-visible:ring-2 focus-visible:outline-none"
+              className="focus-visible:ring-accent focus-visible:ring-offset-primary block rounded-2xl p-2 transition-all duration-200 ease-in-out focus-visible:leading-none focus-visible:ring-2 focus-visible:outline-none"
               to="/"
               aria-label="IMAPI home"
             >
               <img
-                className="mx-auto min-h-4.25 min-w-14.75"
+                className="min-h-4.25 min-w-14.75"
                 src={logo}
                 aria-hidden="true"
               />
@@ -82,7 +82,7 @@ function Header() {
             <li className="flex items-center">
               <div className="flex-center gap-2">
                 <div className="flex flex-col justify-center">
-                  <p className="mb-0.5 text-base leading-none">
+                  <p className="mb-1 text-base leading-none">
                     {user.firstName} {user.lastName}
                   </p>
                   <p className="text-muted text-end text-sm leading-none">
@@ -93,7 +93,7 @@ function Header() {
                   <img src={profile} alt="Your profile" />
                 </div>
               </div>
-              <span className="bg-border-subtle mx-4 h-8 w-0.5"></span>
+              <span className="bg-subtle/20 mx-4 h-8 w-0.5 rounded-full"></span>
               <button
                 className="hover:text-secondary text-muted focus-visible:ring-accent focus-visible:ring-offset-primary cursor-pointer transition-all duration-200 ease-in-out focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none"
                 onClick={() => {
