70ad4b13ba96ba2cacc491f15ae22e2d81a9097f
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2026-02-25 15:55:29 +0100

    style(sign-in): add medium shadow to sign-in form

--- src/routes/public/sign-in/components/SignInForm.tsx
+++ src/routes/public/sign-in/components/SignInForm.tsx
@@ -47,7 +47,7 @@ function SignInForm() {
     <form
       id="submit-form"
       onSubmit={(e) => handleSubmit(e)}
-      className="bg-modal/85 border-subtle/60 flex w-full flex-col gap-6 rounded-3xl border border-solid p-8 px-4"
+      className="bg-modal/85 border-subtle/60 shadow-elevation-medium flex w-full flex-col gap-6 rounded-3xl border border-solid p-8 px-4"
     >
       <div className="flex flex-col gap-2">
         <label className="self-start text-sm font-semibold" htmlFor="username">
